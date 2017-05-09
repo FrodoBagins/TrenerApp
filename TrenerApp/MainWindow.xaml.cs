4921518d01021086ca142dd955735d8ebc9132ec
Izabela Antoniuk <antoniukizabela@gmail.com>
2017-05-09 23:33:31 +0200

    Prowizorka kalendarzyka huhu

--- TrenerApp/MainWindow.xaml.cs
+++ TrenerApp/MainWindow.xaml.cs
@@ -23,6 +23,41 @@ namespace TrenerApp
         public MainWindow()
         {
             InitializeComponent();
+            List<Recipe> recipesList;
+
+            recipesList = new List<Recipe>();
+
+            recipesList.Add(new Recipe()
+            {
+                title = "Schabowy",
+                description = "Mniam mniam pyszne mięsko.",
+                imagePath = "images/schabowe.png"
+            });
+
+            recipesList.Add(new Recipe()
+            {
+                title = "Bigos szlachetny",
+                description = "Kunszt i tradycja.",
+                imagePath = "images/bigos.png"
+            });
+
+            recipes.ItemsSource = recipesList;
         }
+
+        private void DatePicker_SelectedDateChanged(object sender,
+            SelectionChangedEventArgs e)
+        {
+            var picker = sender as DatePicker;
+            DateTime? date = picker.SelectedDate;
+            //if (date == null)
+            //{
+            //    this.Title = "Brak daty.";
+            //}
+            //else
+            //{
+            //    this.Title = date.Value.ToShortDateString();
+            //}
+        }
+
     }
 }
---
+++ TrenerApp/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrenerApp
+{
+    public class Recipe
+    {
+        //tymczasowe to wszystko hue hue hue
+        public string title { get; set; }
+        public string description { get; set; }
+        public string imagePath { get; set; }
+    }
+}
